b4af9f58b86ff4b821bf6c551840d9735ee1c26e
12210193_Antonia_asiu <antoniaasiu02@gmail.com>
2022-10-06 13:16:34 +0700

    migrasi oleh antonia asiu

---
+++ app/Database/Migrations/2022-10-06-053837_Bagian.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Bagian extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'    => ['type'=>'int', 'constraint'=>10, 'unsigned'=>true, 'auto_increment'=>true],
+            'nama'  => ['type'=>'varchar', 'constraint'=>60, 'null'=>true],
+            'fungsi'=> ['type'=>'text', 'null'=>true],
+            'tugas_pokok' => ['type'=>'varchar', 'null'=>true]
+        ]);
+    }
+    public function down()
+    {
+        $this->forge->dropTable('Bagian');
+    }
+}
