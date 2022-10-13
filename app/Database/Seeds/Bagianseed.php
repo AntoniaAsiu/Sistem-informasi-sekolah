ec48970c1b64e3c202fac92354dad760b95470fc
12210193_Antonia_asiu <antoniaasiu02@gmail.com>
2022-10-13 14:42:37 +0700

    Seeds new

--- app/Database/Seeds/Bagianseed.php
+++ app/Database/Seeds/Bagianseed.php
@@ -2,12 +2,19 @@
 
 namespace App\Database\Seeds;
 
+use App\Models\BagianModel;
 use CodeIgniter\Database\Seeder;
 
 class BagianSeed extends Seeder
 {
     public function run()
     {
-        //
+        $id = (new BagianModel())->insert([
+            'nama' => 'Antonia_asiu',
+            'fungsi' => 'mencari',
+            'tugas_pokok' => 'mencatat data',
+        ]);
+        echo "hasil id = $id";
+        
     }
 }
