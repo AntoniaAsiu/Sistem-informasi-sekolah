d5e21e13aac2d59aba33585d2adf19fac03d6c6e
Yogaalriq <113650266+Yogaalriq@users.noreply.github.com>
2022-10-13 08:37:35 +0700

    Add files via upload

---
+++ 2022-09-27-183316_kehadiran_siswa.php
@@ -0,0 +1,18 @@
+php<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Sekolah extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
